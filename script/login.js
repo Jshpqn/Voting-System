06089e329cb19298696e3dcc0685dfa338a45753
Jshpqn <113074374+Jshpqn@users.noreply.github.com>
2023-12-20 11:39:31 +0800

    test login

---
+++ script/login.js
@@ -0,0 +1,33 @@
+const msalConfig = {
+    auth: {
+        clientId: '6a7ae613-d2d6-4d73-adc2-ca052d3ff311',
+        authority: 'https://login.microsoftonline.com/common',
+        redirectUri: 'http://localhost:5500/index.html',
+    },
+    cache: {
+        cacheLocation: "localStorage",
+        storeAuthStateInCookie: true,
+    },
+};
+
+const loginRequest = {
+    scopes: ['openid', 'profile', 'User.Read'],
+};
+
+const msalInstance = new msal.PublicClientApplication(msalConfig);
+
+async function signIn() {
+    try {
+        msalInstance.loginPopup(loginRequest, (error, authResponse) => {
+            if (error) {
+                console.error('Authentication error:', error);
+            } else if (authResponse) {
+                if (authResponse.account) {
+                    console.log("tapos");
+                }
+            }
+        });
+    } catch (error) {
+        console.error('error:', error);
+    }
+}
